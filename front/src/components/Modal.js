e054e002b19fa93cc8b0c53f7f21030bd90e5834
Dominykas Rutkauskas <39955499+domrut@users.noreply.github.com>
2023-08-06 20:20:15 +0300

    Update Modal.js

--- front/src/components/Modal.js
+++ front/src/components/Modal.js
@@ -42,7 +42,7 @@ function Modal({store}) {
 
     return (
         <>
-            <div onClick={() => dispatch(updateModalMenu(!modalStore.modalOpen))} className="top-0 bottom-0 right-0 left-0 z-20 absolute bg-black opacity-70"></div>
+            // <div onClick={() => dispatch(updateModalMenu(!modalStore.modalOpen))} className="top-0 bottom-0 right-0 left-0 z-20 absolute bg-black opacity-70"></div>
             <div className="fixed w-[200px] p-3 2xs:p-0 2xs:w-[300px] z-[21] h-[400px] sm:w-[500px] rounded-md bg-sectionColor dark:bg-sectionColorLight top-[30%] left-0 right-0 m-auto translate-y-[-50%]">
                     {isDataSent ?
                         <div className="text-center mt-[2rem] sm:mt-[4rem]">
