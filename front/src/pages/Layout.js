ddfbd31b68a1c83d25a86d5be84bc68df66b17bb
Dominykas Rutkauskas <39955499+domrut@users.noreply.github.com>
2023-08-07 17:58:44 +0300

    Update Layout.js

--- front/src/pages/Layout.js
+++ front/src/pages/Layout.js
@@ -27,7 +27,7 @@ function Layout({store}) {
                             <Route path="/my-library/" element={<Playlists store={store}/>}/>
                             <Route path="/selectedSongs" element={<SelectedSongsPage store={store}/>}/>
                             <Route path="/:type/:id/tracks" element={<TracksPage store={store}/>}/>
-                            <Route path="/" element={<Navigate to={"/search"}/>}/>
+                            <Route path="/" element={<Navigate to={"/my-library"}/>}/>
                             <Route path="*" element={<Error404/>}/>
                         </>
                     }
